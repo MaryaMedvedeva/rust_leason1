4dc6232416b88f529514161dfee204cf99dbcb98
gleb226 <gmedvede226@gmail.com>
2023-07-27 20:07:31 +0300

    praktis

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
 fn main() {
-    let mut name = String::from("Sasha");
-    println!("My name is {}", name)
+    let Surname = String::from("Zelensky");
+    println!("{}", Surname);
 }
-
-// u8 u16 u32 u64 u128 usize
-// i8 i16 i32 i64 i128 isize
-
-// -3 -7 -124 -223
