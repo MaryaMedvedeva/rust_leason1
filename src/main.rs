112d0d67caff0a820fa1d7c062f1e3e3074b5db4
gleb226 <gmedvede226@gmail.com>
2023-07-27 19:12:19 +0300

    variables, no mutable variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!")
+    let age:i8 = 23;
+    age = 25;
+    println!("My age is {}", age);
 }
+
+// integer 1 6 7 125 231 333
+// float 1.25 3.67 237.0 7.45
+
+// i8 i16 i32 i64 i128 isize
+
+// mutable
