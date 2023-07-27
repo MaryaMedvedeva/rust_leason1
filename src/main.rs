4ddf185126c3c06aeda321d550e74508f9287775
gleb226 <gmedvede226@gmail.com>
2023-07-27 20:49:10 +0300

    two line comment

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    let mut age = 13;
-    let mut name = String::from("Hlieb");
-    println!("Hi, my name is {}, i`m {} years old", name, age)
+    /*Hi
+    Hello
+    Bye
+    Goodbye
+     */
 }
