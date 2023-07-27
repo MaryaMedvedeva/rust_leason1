8b9ce5f74f4318d3040cb7b1fbe850cddc505bfc
gleb226 <gmedvede226@gmail.com>
2023-07-27 19:38:34 +0300

    float numbers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let mut age:i8 = 23;
-    age = 25;
-    println!("My age is {}", age);
+    let mut num: f64 = 345.73;
+    num = 672.491;
+    println!("Number is {}", num);
 }
 
 // integer 1 6 7 125 231 333
-// float 1.25 3.67 237.0 7.45
+// float 1.25 3.678 237.0 7.45
 
 // i8 i16 i32 i64 i128 isize
 
