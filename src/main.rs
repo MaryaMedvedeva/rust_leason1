d11e4d15f97181bea5bf71bb95cdf9caef4b42cd
gleb226 <gmedvede226@gmail.com>
2023-07-28 21:38:45 +0300

    match 2

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
 fn main() {
-    // mathch
+    // match
 
+    let numx =3;
 
+    let num = match numx {
+        2 => 1,
+        3 => 10,
+        3..=10 => 7,
+        _ => 0
+    };
 
-    let num = 87;
-
-    match num {
-        10 => println!("Num is 10"),
-        23 => {
-            println!("Num is 23");
-            println!("Num is matched!")
-        },
-        0..=50 => {
-            println!("Number betwen 0 and 50");
-        },
-        50..=100 => {
-          println!("Number betven 50 and 100")
-        },
-        _ => {
-            println!("No variants!");
-            println!("Try again!");
-        }
-    }
+    println!("{}", num);
 }
 
