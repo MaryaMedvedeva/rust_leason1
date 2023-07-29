dbda150efef17a85cfe5480f960ef0518e6099d0
gleb226 <gmedvede226@gmail.com>
2023-07-29 22:45:46 +0300

    io 2, quadratic equation

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,57 @@
+use std::intrinsics::transmute;
 use std::io;
 
 fn main() {
-    // io
-    // i - input
-    // o - output
+    // ax^2 + bx + c = 0
+    // D = b^2 - 4(a*c)
 
-    let mut name = String::new();
+    let mut a_str = String::new();
+    let mut b_str = String::new();
+    let mut c_str = String::new();
 
-    println!("PLese, write your name: ");
+    loop {
+        println!("Solve quadratic equation");
 
-    match  io::stdin().read_line(&mut name){
-         Ok(_) => {
-            println!("Hello, {}", name);
-         },
-        Err(e) => {
-            println!("PROGRAM ERROR - {}", e);
+        println!("Write a:");
+        match io::stdin().read_line(&mut a_str) {
+            Ok(_) => {},
+            Err(e) => println!("Input error - {}", e)
+        }
+
+        println!("Write b:");
+        match io::stdin().read_line(&mut b_str) {
+            Ok(_) => {},
+            Err(e) => println!("Input error - {}", e)
+        }
+
+        println!("Write c:");
+        match io::stdin().read_line(&mut c_str) {
+            Ok(_) => {},
+            Err(e) => println!("Input error - {}", e)
+        }
+
+        let a: f64 = a_str.trim().parse().unwrap();
+        let b: f64 = b_str.trim().parse().unwrap();
+        let c: f64 = c_str.trim().parse().unwrap();
+
+        let d: f64 = (b * b) - 4.0 * (a * c);
+
+        if d > 0.0 {
+            let x1 = ((-b) + d.sqrt()) / (2.0 * a);
+            let x2 = ((-b) - d.sqrt()) / (2.0 * a);
+
+            println!("Answer\nThere are two roots\nD = {}\nRoot 1 = {}\nRoot 2 = {}", d, x1, x2);
+        }
+        if d == 0.0 {
+            let x = (-b) / (2.0 * a);
+
+            println!("Answer\nThere are one roots\nD = 0\nRoot = {}", x);
+        }
+
+        if d < 0.0 {
+            println!("Roots don't exist\nD < 0\n = {}", d);
         }
     }
+
 }
 
